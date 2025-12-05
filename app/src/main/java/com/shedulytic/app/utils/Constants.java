b70de1c2077079b0d41c45ae61f4735b90ec90a0
Your Name <you@example.com>
2025-12-05 10:39:17 +0530

    Remove unused duplicate com.shedulytic.app package
    The main app uses com.example.shedulytic package exclusively.
    The com.shedulytic.app package contained duplicate files that were not used:
    - AddHabitActivity.java (duplicate)
    - HabitAdapter.java (duplicate)
    - HabitFragment.java (duplicate)
    - Habit.java (duplicate)
    - Constants.java (not referenced by main app)
    
    Keeping only com.example.shedulytic as the single package.

--- app/src/main/java/com/shedulytic/app/utils/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.shedulytic.app.utils;
-
-public class Constants {    // Base URL for API endpoints - should match IpV4Connection.java
-    public static final String BASE_URL = "http://10.95.189.64/shedulytic/";
-} 
